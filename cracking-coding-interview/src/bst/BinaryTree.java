e1e26ebf8c9620c25882c387d2b7d4040d836e9c
mlabouardy <mohamed@labouardy.com>
2016-05-12 15:32:28 +0100

    Deck of cards

--- cracking-coding-interview/src/bst/BinaryTree.java
+++ cracking-coding-interview/src/bst/BinaryTree.java
@@ -6,54 +6,65 @@ public class BinaryTree {
     private Node root;
 
     public Node insert(int data){
-    	Node element=new Node(data);
-    	if(root==null){
-    		root=element;
-    	}else{
-    		Node tmp,prev;
-    		tmp=prev=root;
-    		while(tmp!=null){
-    			prev=tmp;
-    			if(tmp.getData() > data)
-    				tmp=tmp.getLeft();
-    			else
-    				tmp=tmp.getRight();
-    		}
-    		if(prev.getData() > data)
-    			prev.setLeft(element);
-    		else
-    			prev.setRight(element);
-    	}
-    	return element;
+        Node element=new Node(data);
+        if(root==null){
+            root=element;
+        }else{
+            Node tmp,prev;
+            tmp=prev=root;
+            while(tmp!=null){
+                prev=tmp;
+                if(tmp.getData() > data)
+                    tmp=tmp.getLeft();
+                else
+                    tmp=tmp.getRight();
+            }
+            if(prev.getData() > data)
+                prev.setLeft(element);
+            else
+                prev.setRight(element);
+        }
+        return element;
     }
 
     public void inorder(Node focusNode){
         if(focusNode!=null){
-        	inorder(focusNode.getLeft());
-        	System.out.print(focusNode.getData()+" ");
-        	inorder(focusNode.getRight());
+            inorder(focusNode.getLeft());
+            System.out.print(focusNode.getData()+" ");
+            inorder(focusNode.getRight());
         }
     }
 
     public Node constructBST(int[] input){
-    	Node tree=new Node();
-    	return construct(tree, input, 0, input.length);
+        Node tree=new Node();
+        return construct(tree, input, 0, input.length);
     }
 
     public Node construct(Node tree, int[] input, int min, int max){
-    	if(min>=max)
-    		return null;
-    	int middle=(min+max)/2;
-    	int element=input[middle];
-    	Node e=insert(element);
+        if(min>=max)
+            return null;
+        int middle=(min+max)/2;
+        int element=input[middle];
+        Node e=insert(element);
 
-    	construct(e.getLeft(), input, min, middle);
-    	construct(e.getRight(), input, middle+1, max);
+        construct(e.getLeft(), input, min, middle);
+        construct(e.getRight(), input, middle+1, max);
 
-    	return e;
+        return e;
+    }
+
+    //is t1 subtree of t2
+    public boolean isSubtree(Node t1, Node t2){
+        if(t1==null)
+            return true;
+        if(t2==null)
+            return false;
+        if(t1.getData() == t2.getData())
+            return true;
+        return isSubtree(t1, t2.getLeft()) || isSubtree(t1, t2.getRight());
     }
 
     public Node getRoot(){
-    	return root;
+        return root;
     }
 }
--- cracking-coding-interview/src/test/Main.java
+++ cracking-coding-interview/src/test/Main.java
@@ -1,20 +1,34 @@
 package test;
 
+import java.util.Locale.Category;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 import bst.BinaryTree;
 import bst.Node;
+import deckcards.Card;
+import deckcards.CardV2;
+import deckcards.Club;
+import deckcards.Diamond;
+import deckcards.Heart;
+import deckcards.Spade;
 import graph.Graph;
 import graph.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
-		int[] input={1,2,3};
-		BinaryTree tree=new BinaryTree();
-		Node root=tree.constructBST(input);
-		tree.inorder(root);
+		Stream.of('A','2','3','4','5','6','7','8','9','J','K','Q')
+			  .forEach(c -> {
+				  Card card=new Club(c);
+				  System.out.print(card+" ");
+				  card=new Diamond(c);
+				  System.out.print(card+" ");
+				  card=new Heart(c);
+				  System.out.print(card+" ");
+				  card=new Spade(c);
+				  System.out.println(card);
+			  });
 
 	}
 
